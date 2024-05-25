d0b8f282e0663d1a86a31fffe0dd2ba2d6d2558b
Avaya Sharma <avaya.sharma933@gmail.com>
2024-05-25 23:51:39 +0530

    added slices and reducers

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+
+const store = configureStore({
+  reducer:rootReducer,
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+
+  <Provider store={store}>
+
   <BrowserRouter>
   <App />
   </BrowserRouter>
+
+  </Provider>
+  
    
   </React.StrictMode>
 );
