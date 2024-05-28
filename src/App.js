3ce73767b46a0fd139a0d6a312a4514db153510b
Avaya Sharma <avaya.sharma933@gmail.com>
2024-05-29 04:09:55 +0530

    tab and login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup"
+import OpenRoute from "./components/core/Auth/OpenRoute"
 
 
 function App() {
@@ -11,7 +14,23 @@ function App() {
    <Navbar/>
     <Routes>
     <Route path="/" element={<Home/>} />
-    
+
+    <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup/>
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login/>
+            </OpenRoute>
+          }
+        />
 
 
 
