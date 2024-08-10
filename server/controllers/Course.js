9fb9f4df6e5e3a1f5ee346783a45a2c77e6b3273
Avaya Sharma <avaya.sharma933@gmail.com>
2024-08-10 23:53:54 +0530

    controller changes

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -16,15 +16,21 @@ let {
     courseDescription,
     whatYouWillLearn,
     price,
-    // tag,
+    tag : _tag,
     category,
     status,
-    instructions,
+    instructions : _instructions,
 } = req.body;
 
     // get thumbnail
     const thumbnail = req.files.thumbnailImage;
-    console.log(thumbnail);
+
+    // Convert the tag and instructions from stringified Array to Array
+    const tag = JSON.parse(_tag)
+    const instructions = JSON.parse(_instructions)
+
+    console.log("tag", tag)
+    console.log("instructions", instructions)
 
     // Check if any of the required fields are missing
     if (
@@ -32,7 +38,7 @@ let {
       !courseDescription ||
       !whatYouWillLearn ||
       !price ||
-      // !tag ||
+      !tag.length ||
       !thumbnail ||
       !category  ||
       !instructions
@@ -41,6 +47,10 @@ let {
         success: false,
         message: "All Fields are Mandatory",
       });
+    }
+
+    if (!status || status === undefined) {
+      status = "Draft"
     }
 
     // check for instructor
@@ -60,13 +70,7 @@ let {
       });
     }
 
-      // Convert category to ObjectId if it is not already
-    //   if (!mongoose.Types.ObjectId.isValid(category)) {
-    //     return res.status(400).json({
-    //         success: false,
-    //         message: "Invalid Category ID",
-    //     });
-    // }
+     
 
     // //check given tag is valid or not
     console.log(category);
@@ -100,11 +104,11 @@ let {
       instructor: instructorDetails._id,
       whatWillYouLearn : whatYouWillLearn,
       price,
-      // tag: tag,
+       tag,
       category : categoryDetails._id,
       thumbnail: thumbnailImage.secure_url,
       status : status,
-      instructions : instructions,
+       instructions,
     });
     //add the new course to the user Schema of intructor
     await User.findByIdAndUpdate(
@@ -149,12 +153,15 @@ let {
   }
 };
 
+
 //getAll courses handler function
 exports.getAllCourses = async (req, res) => {
   //DOUBT
   try {
     const allCourses = await Course.find(
-      {},
+      // {},   Earlier
+
+      { status: "Published" },   //babbar added afterwards 
       {
         //DOUBT
         courseName: true, //DOUBT
@@ -208,6 +215,7 @@ exports.getCourseDetails = async (req, res) => {
         path: "courseContent", //courseContent is ref to Sections and Sections is ref to subSections
         populate: {
           path: "subSection",
+          select: "-videoUrl",
         },
       })
       .exec();
@@ -219,10 +227,30 @@ exports.getCourseDetails = async (req, res) => {
       });
     }
 
+     // if (courseDetails.status === "Draft") {
+    //   return res.status(403).json({
+    //     success: false,
+    //     message: `Accessing a draft course is forbidden`,
+    //   });
+    // }
+
+    let totalDurationInSeconds = 0                                  //Added afterwards
+    courseDetails.courseContent.forEach((content) => {                         //Added afterwards
+      content.subSection.forEach((subSection) => {                         //Added afterwards
+        const timeDurationInSeconds = parseInt(subSection.timeDuration)                         //Added afterwards
+        totalDurationInSeconds += timeDurationInSeconds                         //Added afterwards
+      })                         //Added afterwards
+    })                         //Added afterwards
+
+    const totalDuration = convertSecondsToDuration(totalDurationInSeconds)                         //Added afterwards
+
+
     return res.status(200).json({
-      success: true,
-      message: "Course details fetched successfully",
-      data: courseDetails,
+    success: true,
+      data: {
+        courseDetails,
+        totalDuration,
+      },
     });
   } catch (error) {
     return res.status(500).json({
@@ -231,3 +259,76 @@ exports.getCourseDetails = async (req, res) => {
     });
   }
 };
+
+
+
+// Edit Course Details
+exports.editCourse = async (req, res) => {
+  try {
+    const { courseId } = req.body
+    const updates = req.body
+    const course = await Course.findById(courseId)
+
+    if (!course) {
+      return res.status(404).json({ error: "Course not found" })
+    }
+
+    // If Thumbnail Image is found, update it
+    if (req.files) {
+      console.log("thumbnail update")
+      const thumbnail = req.files.thumbnailImage
+      const thumbnailImage = await uploadImageToCloudinary(
+        thumbnail,
+        process.env.FOLDER_NAME
+      )
+      course.thumbnail = thumbnailImage.secure_url
+    }
+
+    // Update only the fields that are present in the request body
+    for (const key in updates) {
+      if (updates.hasOwnProperty(key)) {
+        if (key === "tag" || key === "instructions") {
+          course[key] = JSON.parse(updates[key])
+        } else {
+          course[key] = updates[key]
+        }
+      }
+    }
+
+    await course.save()
+
+    const updatedCourse = await Course.findOne({
+      _id: courseId,
+    })
+      .populate({
+        path: "instructor",
+        populate: {
+          path: "additionalDetails",
+        },
+      })
+      .populate("category")
+      .populate("ratingAndReviews")
+      .populate({
+        path: "courseContent",
+        populate: {
+          path: "subSection",
+        },
+      })
+      .exec()
+
+    res.json({
+      success: true,
+      message: "Course updated successfully",
+      data: updatedCourse,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    })
+  }
+}
+
+
